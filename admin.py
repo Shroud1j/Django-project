6648f89922cb8eabee620e3eb725e2d2d01fe53a
Dhananjay Shinde | AI & Data Science Innovator <dhananjayms2005@gmail.com>
2026-08-12 11:50:21 +0530

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,40 +1,73 @@
 from django.contrib import admin
-from .models import Course, Lesson, Question, Choice, Submission
+from .models import (
+    Course,
+    Lesson,
+    Instructor,
+    Learner,
+    Question,
+    Choice,
+    Submission
+)
 
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
-    extra = 2
+    extra = 1
 
 
 class QuestionInline(admin.StackedInline):
     model = Question
     extra = 1
-    inlines = [ChoiceInline]
+    show_change_link = True
 
 
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ("text", "lesson")
+    list_display = (
+        "question_text",
+        "course",
+        "lesson",
+        "grade"
+    )
     inlines = [ChoiceInline]
 
 
 @admin.register(Lesson)
 class LessonAdmin(admin.ModelAdmin):
-    list_display = ("title", "course")
+    list_display = (
+        "title",
+        "course"
+    )
     inlines = [QuestionInline]
 
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ("name",)
+    list_display = ("name", "description")
+
+
+@admin.register(Instructor)
+class InstructorAdmin(admin.ModelAdmin):
+    list_display = ("user",)
+
+
+@admin.register(Learner)
+class LearnerAdmin(admin.ModelAdmin):
+    list_display = ("user",)
 
 
 @admin.register(Choice)
 class ChoiceAdmin(admin.ModelAdmin):
-    list_display = ("text", "is_correct", "question")
+    list_display = (
+        "choice_text",
+        "question",
+        "is_correct"
+    )
 
 
 @admin.register(Submission)
 class SubmissionAdmin(admin.ModelAdmin):
-    list_display = ("user", "question", "choice")
+    list_display = (
+        "learner",
+        "course"
+    )
